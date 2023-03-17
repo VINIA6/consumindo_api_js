cdf5e85cfefe1c416ec910531229dba2df7dfccb
VINIA6 <viniciusdeassisazevedo@hotmail.com>
2023-03-17 17:14:55 -0300

    feat: developing promises

---
+++ consuming_api_axios/promises/main.js
@@ -0,0 +1,68 @@
+const url = 'http://localhost:5500/api'
+
+function getUsers() {
+    axios.get(url)
+    .then(response=>{
+        // Eu usei a variavel criado na div e estou pasasndo por meio da DOM o response da API 
+        apiResult.textContent = JSON.stringify(response.data)
+    })
+    .catch(error=>console.error(error))
+}
+
+function addNewUser(newUser){
+    axios.post(url,newUser)
+    .then(response=>{
+        console.log(response)
+    })
+    .catch(error=>console.log(error))
+}
+
+function getUser(id){
+    axios.get(`${url}/${id}`)
+    .then(response=> {
+
+        const data = response.data
+
+        userName.textContent = data.name
+        userCity.textContent = data.city
+        userId.textContent = data.id
+        userAvatar.src = data.avatar
+
+    })
+    .catch(error => console.error(error))
+}
+
+function updateUser(id,userUpdated){
+    axios.put(`${url}/${id}`, userUpdated)
+    .then(resp =>{
+        console.log(resp)
+    })
+    .catch(error=>console.error(error))
+}
+
+function deleteUser(id){
+    axios.delete(`${url}/${id}`)
+    .then(resp =>{
+        console.log(resp)
+    })
+    .catch(error=>console.error(error))
+}
+
+const userUpdated = {
+    name:"Lucia Jacinta",
+    avatar:"https://picsum.photos/200/300",
+    city: "Russas",
+}
+
+// updateUser(1, userUpdated)
+// deleteUser(1)
+// deleteUser(1)
+getUsers()
+getUser(2)
+
+const newUser = {
+    name:"Vinícius",
+    avatar:"https://picsum.photos/200/300",
+    city: "Limoeiro do Norte",
+}
+// addNewUser(newUser)
